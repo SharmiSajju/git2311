d1fbbccf71b1935fc5770105ffad16ba7c21a442
Sharmisajju <sharmimahaboob@gmail.com>
2021-11-23 22:03:26 +0530

    First stage

---
+++ src/org/programjava/TestGit.java
@@ -0,0 +1,20 @@
+package org.programjava;
+
+public class TestGit {
+	
+	private void name() {
+	System.out.println("name is ram");
+		}
+	private void age() {
+		System.out.println("age is 25");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		TestGit obj= new TestGit();
+		obj.name();
+		obj.age();
+	}
+
+}
